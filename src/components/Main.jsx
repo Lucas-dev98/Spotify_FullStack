3bc971b3421c8005cefea62bf6f0871c93b4d42d
Lucas-dev98 <l.o.bastos@live.com>
2025-05-08 22:57:24 -0300

    Reinicializando repositório local

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ItemList from "./ItemList";
+
+const Main = () => {
+  return (
+    <div className="main">
+      {/* Item List de Artistas */}
+      <ItemList  title="Artistas"/>
+      
+      {/* Item List de Musicas */}
+      <ItemList title="Musicas"/>
+    </div>
+  );
+};
+
+export default Main;
