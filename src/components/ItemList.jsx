97bd71144c192e96daf28ec6a11a0c4ea7e7152b
lucas-dev98 <l.o.bastos@live.com>
2025-05-11 22:37:43 -0300

    Adding new features and navigation flow between antists and songs artists and player

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import SingleItem from './SingleItem';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const ItemList = ({ title, items, itemsArray, path, idPath}) => {
-  console.log(title, items);
+const ItemList = ({ title, items, itemsArray, path, idPath }) => {
+  // console.log(title, items);
+  // console.log(useLocation());
+  const { pathname } = useLocation();
+  // console.log(pathname);
+  const isHome = pathname === '/';
+  const finalItems = isHome ? items : Infinity ;
   return (
     <div className="item-list">
       <div className="item-list__header">
-        <h2>{title}{title} populares</h2>
-        <Link to={path} className="item-list__link">
-          Mostrar tudo
-        </Link>
+        <h2>{title} populares</h2>
+
+        {isHome ? (
+          <Link to={path} className="item-list__link">
+            Mostrar tudo
+          </Link>
+        ) : (
+          <></>
+        )}
       </div>
 
       <div className="item-list__container">
         {itemsArray
-          .filter((currentValue, index) => index < items)
+          .filter((currentValue, index) => index < finalItems)
           .map((currObj, index) => (
             <SingleItem
               // id={currObj.id}
--- src/pages/Songs.jsx
+++ src/pages/Songs.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import Main from '../components/Main';
 
 const Songs = () => {
-  return (
-    <div>Songs</div>
-  )
+  return <Main type="songs"/>
+  
 }
 
 export default Songs
